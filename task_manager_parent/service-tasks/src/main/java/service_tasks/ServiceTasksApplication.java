a098c73fa55d27f12c8d7b42b208ddd59c537f0b
Muzza1103 <m.muzzammil02@hotmail.com>
2025-04-20 21:31:27 +0200

    Initialization of the parent class / 2 child classes

---
+++ task_manager_parent/service-tasks/src/main/java/service_tasks/ServiceTasksApplication.java
@@ -0,0 +1,13 @@
+package service_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTasksApplication.class, args);
+	}
+
+}
